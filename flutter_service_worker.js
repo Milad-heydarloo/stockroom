8491318416726e5bd051c3fb1bf56199d935d8d5
Milad-heydarloo <phone.sync.heydarloo@gmail.com>
2024-09-17 11:27:37 +0000

    deploy: a0204039cb8f180b30efe1d0f9c2610ca135b9ab

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"main.dart.js": "9761f5562f65df1da248cc525daa72eb",
-"index.html": "6758f4aa442521d7e34d3ff7edc28c07",
-"/": "6758f4aa442521d7e34d3ff7edc28c07",
+"index.html": "324a0e3a763897b16b5ff6373d2ccf0d",
+"/": "324a0e3a763897b16b5ff6373d2ccf0d",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
