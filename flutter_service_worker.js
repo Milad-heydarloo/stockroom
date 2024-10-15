d47ff8bfc41e67b47bfc1bfaa9bd233b4eca1cb7
Milad-heydarloo <phone.sync.heydarloo@gmail.com>
2024-10-15 08:52:11 +0000

    deploy: e44a49ee41c98bc9eaea9b60ccffd2bed96fdafa

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "c89cc3da0685ead3d88892016bc69308",
-"/": "c89cc3da0685ead3d88892016bc69308",
+const RESOURCES = {"index.html": "b791c67954b98d82ff16425dc10a92dc",
+"/": "b791c67954b98d82ff16425dc10a92dc",
 "favicon.png": "2ce08210fe3dea224fb9ece8f274c7ce",
 "canvaskit/skwasm.wasm": "d1fde2560be92c0b07ad9cf9acb10d05",
 "canvaskit/skwasm.worker.js": "51253d3321b11ddb8d73fa8aa87d3b15",
